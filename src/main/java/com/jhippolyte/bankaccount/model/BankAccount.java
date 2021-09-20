74f196358dbc10d08faf2414055bf1d81f8e2ec0
LearningCitizen <johnson.hippolyte@yahoo.fr>
2021-09-20 14:15:16 +0200

    add jpa configuration in BankAccount

--- src/main/java/com/jhippolyte/bankaccount/model/BankAccount.java
+++ src/main/java/com/jhippolyte/bankaccount/model/BankAccount.java
@@ -3,15 +3,34 @@ package com.jhippolyte.bankaccount.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "bankAccounts")
 public class BankAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "owner")
     private Person owner;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "balance")
     private Double balance = 50.00; //An account is created initially with 50 EUROS
+
+    @Column(name = "type")
     private AccountType type;
+
+    @Column(name = "overdraft")
     private Double overdraft = 0.00; //No overdraft allowed
 
     public void withdraw(double amount){
