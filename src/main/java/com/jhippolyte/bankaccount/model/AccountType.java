bf47f653d79dfae9835ce07330bb00b238f2cebe
LearningCitizen <johnson.hippolyte@yahoo.fr>
2021-09-20 15:00:09 +0200

    Update AccountType

--- src/main/java/com/jhippolyte/bankaccount/model/AccountType.java
+++ src/main/java/com/jhippolyte/bankaccount/model/AccountType.java
@@ -1,6 +1,13 @@
 package com.jhippolyte.bankaccount.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum AccountType {
-    Current,
-    Saving
+    Current("current"),
+    Saving("saving");
+
+    private String type;
 }
