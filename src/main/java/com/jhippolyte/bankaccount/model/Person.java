c7f9d6684e240097d9d301754b83f1489355ab5e
LearningCitizen <johnson.hippolyte@yahoo.fr>
2021-09-20 14:10:32 +0200

    add jpa configuration in Person

--- src/main/java/com/jhippolyte/bankaccount/model/Person.java
+++ src/main/java/com/jhippolyte/bankaccount/model/Person.java
@@ -3,13 +3,28 @@ package com.jhippolyte.bankaccount.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
+
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "persons")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "firstName")
     private String firstName;
+
+    @Column(name = "lastName")
     private String lastName;
+
+    @Column(name = "mail")
     private String mail;
 
 }
