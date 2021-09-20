3527539181b1f22dc6f1aa0c19167917125a4d15
LearningCitizen <johnson.hippolyte@yahoo.fr>
2021-09-20 11:20:00 +0200

    create Person class

---
+++ src/main/java/com/jhippolyte/bankaccount/model/Person.java
@@ -0,0 +1,15 @@
+package com.jhippolyte.bankaccount.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String mail;
+
+}
